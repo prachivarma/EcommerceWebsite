8369a1d2216f5c16533bdb089ec92a072ead3f93
Nikesh <nikeshvarma50@gmail.com>
2020-10-07 23:05:37 +0530

    Initial commit

---
+++ static/js/HomeJS.js
@@ -0,0 +1,42 @@
+function changeImage(_src) {
+    var imgDiv = document.getElementById('main-img');
+    var imageElement = imgDiv.getElementsByTagName('img')[0];
+    imageElement.src = _src;
+}
+
+$(function () {
+    $('a').each(function () {
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('active');
+            $(this).parents('li').addClass('active-link');
+        }
+    });
+});
+
+
+function searchQuery(event) {
+    $('#search-result').html('');
+    $.ajax({
+        url: '/search-items/',
+        method: 'POST',
+        dataType: 'json',
+        data: {'query': event.target.value},
+        success: function (data) {
+            if (data['response'] != 'None') {
+                $.each(data, function (key, value) {
+                    $('#search-result').append(
+                        `
+                            <a id="search-anchor" class="anchor-class" href="http://localhost:8000/view-product-info/${value[0]}/">
+                                <li class="list-group-item search-list">
+                                    <img class="search-img" src='/media/${value[2]}' alt="${value[0]}"> <h6 class="search-h1"> ${value[1]}</h6>
+                                </li>
+                            </a>
+                        `
+                    )
+                })
+            } else {
+                $('#search-result').html('');
+            }
+        }
+    })
+}
