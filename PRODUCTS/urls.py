76d32bcbea491d9b5b7c624ab66e9d82a008e50c
Nikesh <nikeshvarma50@gmail.com>
2020-10-10 23:55:42 +0530

    1.0.3

--- PRODUCTS/urls.py
+++ PRODUCTS/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('list-product/', views.ListProductView.as_view(), name='list_product'),
+    path('inventory/', views.InventoryListView.as_view(), name='inventory_page'),
 ]
--- PRODUCTS/views.py
+++ PRODUCTS/views.py
@@ -1,9 +1,30 @@
-from django.views.generic import CreateView
+from django.contrib import messages
+from django.urls import reverse
+from django.views.generic import CreateView, ListView
+from django.contrib.auth import get_user_model
 from .models import Product
 from .forms import ListProductForm
+
+User = get_user_model()
 
 
 class ListProductView(CreateView):
     template_name = 'products/listing_page.html'
     queryset = Product.objects.all()
     form_class = ListProductForm
+
+    def form_valid(self, form):
+        postForm = form.save(commit=False)
+        postForm.product_shop_id = self.request.user.shop.id
+        postForm.save()
+        messages.success(self.request, 'Product added to inventory.')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('list_product')
+
+
+class InventoryListView(ListView):
+    template_name = 'products/inventory_page.html'
+    model = Product
+    paginate_by = 20
